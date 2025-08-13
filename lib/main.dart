3392907e379fe8ae28b0f3414f5d89c045791640
azul <life.89.blue@gmail.com>
2025-08-13 14:38:02 -0300

    update/initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:azul_crm/features/auth/screens/login_screen.dart';
 import 'package:azul_crm/theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(
+    MaterialApp(
       title: 'Azul CRM',
       theme: AppTheme.lightTheme,
       home: const LoginScreen(),
-  ));
+    ),
+  );
 }
