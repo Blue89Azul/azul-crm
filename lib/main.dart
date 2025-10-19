ca89b61bb213daf92874f36d6ff0d553410a31a1
azul <life.89.blue@gmail.com>
2025-10-19 15:01:12 -0300

    update/implemented go_router for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,13 @@
-import 'package:azul_crm/features/auth/screens/login_screen.dart';
-import 'package:azul_crm/features/auth/screens/signup_screen.dart';
+import 'package:azul_crm/core/router/app_router.dart';
 import 'package:azul_crm/theme/app_theme.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+void main() {
   runApp(
-    MaterialApp(
+    MaterialApp.router(
       title: 'Azul CRM',
       theme: AppTheme.lightTheme,
-      home: const LoginScreen(),
-      routes: <String, WidgetBuilder> {
-        '/signup': (BuildContext context) => SignUpScreen(),
-      },
+      routerConfig: appRouter,
     ),
   );
 }
