84595aebba5e456ccfdc2a239f0522de5e84fc35
azul <life.89.blue@gmail.com>
2025-10-19 15:00:42 -0300

    add/created app router

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,59 @@
+import 'package:azul_crm/features/auth/screens/choice_role_screen.dart';
+import 'package:azul_crm/features/auth/screens/login_screen.dart';
+import 'package:azul_crm/features/auth/screens/signup_screen.dart';
+import 'package:azul_crm/features/dashboard/screens/dashboard_view.dart';
+import 'package:azul_crm/features/welcome_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: AppRoutePaths.welcome,
+  routes: <RouteBase>[
+    GoRoute(
+      name: AppRouteNames.dashboard,
+      path: AppRoutePaths.dashboard,
+      builder: (context, state) => DashBoardView(),
+    ),
+    GoRoute(
+      name: AppRouteNames.welcome,
+      path: AppRoutePaths.welcome,
+      builder: (context, state) => WelcomeScreen(),
+    ),
+    GoRoute(
+      name: AppRouteNames.signup,
+      path: AppRoutePaths.signup,
+      builder: (context, state) => SignUpScreen(),
+    ),
+    GoRoute(
+      name: AppRouteNames.login,
+      path: AppRoutePaths.login,
+      builder: (context, state) => LoginScreen(),
+    ),
+    GoRoute(
+      name: AppRouteNames.choiceRole,
+      path: AppRoutePaths.choiceRole,
+      builder: (context, state) => ChoiceRoleScreen(),
+    )
+  ]
+);
+
+
+
+/// Route Names
+abstract class AppRouteNames {
+  static const String choiceRole = 'choice_role';
+  static const String dashboard  = 'dashboard';
+  static const String welcome    = 'welcome';
+  static const String signup     = 'signup';
+  static const String login      = 'login';
+  static const String myPage     = 'myPage';
+}
+
+/// Route Paths
+abstract class AppRoutePaths {
+  static const String choiceRole = '/choice_role';
+  static const String dashboard  = '/dashboard';
+  static const String welcome    = '/welcome';
+  static const String signup     = '/signup';
+  static const String login      = '/login';
+  static const String myPage     = '/my_page';
+}
