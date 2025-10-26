2379b33eaae6483867527a27c45f7840dbe10d7f
azul <life.89.blue@gmail.com>
2025-10-26 11:12:30 -0300

    update/Add JwtRepository to save tokens

---
+++ lib/features/auth/data/repositories/jwt_repository.dart
@@ -0,0 +1,21 @@
+import 'package:azul_crm/core/storage/app_local_storage.dart';
+import 'package:azul_crm/features/auth/data/dto/jwt_token.dart';
+import 'package:dartz/dartz.dart';
+import 'jwt_repository_interface.dart';
+
+class JwtRepository implements JwtRepositoryInterface {
+  static const String _tokenKey = 'json_web_token';
+  final AppLocalStorage _storage;
+
+  JwtRepository(this._storage);
+
+  @override
+  Future<Either<String, void>> saveToken(JwtToken token) async {
+    try {
+      await _storage.write(key: _tokenKey, value: token);
+      return right(null);
+    } catch (e) {
+     return left(e.toString());
+    }
+  }
+}
---
+++ lib/features/auth/data/repositories/jwt_repository_interface.dart
@@ -0,0 +1,7 @@
+import 'package:azul_crm/features/auth/data/dto/jwt_token.dart';
+import 'package:dartz/dartz.dart';
+
+abstract interface class JwtRepositoryInterface {
+  /// Save JWT token to local storage
+  Future<Either<String, void>> saveToken(JwtToken token);
+}
