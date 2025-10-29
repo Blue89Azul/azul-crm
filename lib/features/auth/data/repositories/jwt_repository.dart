34796548717109cb93ab7ed1dd5cffd8ca8fd35c
azul <life.89.blue@gmail.com>
2025-10-29 10:43:02 -0300

    update/Store JWT as JSON string

--- lib/features/auth/data/repositories/jwt_repository.dart
+++ lib/features/auth/data/repositories/jwt_repository.dart
@@ -1,10 +1,12 @@
+import 'dart:convert';
+
 import 'package:azul_crm/core/storage/app_local_storage.dart';
 import 'package:azul_crm/features/auth/data/dto/jwt_token.dart';
 import 'package:dartz/dartz.dart';
 import 'jwt_repository_interface.dart';
 
 class JwtRepository implements JwtRepositoryInterface {
-  static const String _tokenKey = 'json_web_token';
+  static const String _tokenKey = 'jwt';
   final AppLocalStorage _storage;
 
   JwtRepository(this._storage);
@@ -12,7 +14,7 @@ class JwtRepository implements JwtRepositoryInterface {
   @override
   Future<Either<String, void>> saveToken(JwtToken token) async {
     try {
-      await _storage.write(key: _tokenKey, value: token);
+      await _storage.write(key: _tokenKey, value: jsonEncode(token.toJson()));
       return right(null);
     } catch (e) {
      return left(e.toString());
