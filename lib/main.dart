5246a1fe0c4889dedd2598260968ca986fd76a57
azul <life.89.blue@gmail.com>
2025-11-20 12:09:59 -0300

    update/initialize dependency injection in main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:azul_crm/core/di/injector.dart';
 import 'package:azul_crm/core/router/app_router.dart';
 import 'package:azul_crm/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupDependencies();
   runApp(
     MaterialApp.router(
       title: 'Azul CRM',
