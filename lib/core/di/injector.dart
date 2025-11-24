cfad84f0d35ba969843b5d9c12817fa24835490d
azul <life.89.blue@gmail.com>
2025-11-24 11:36:26 -0300

    update/setup AppLogger and inject JwtRepository into AppApiClient in dependency injection setup

--- lib/core/di/injector.dart
+++ lib/core/di/injector.dart
@@ -1,14 +1,22 @@
 import 'package:azul_crm/core/di/modules/auth/auth_module.dart';
+import 'package:azul_crm/core/log/app_logger.dart';
 import 'package:azul_crm/core/network/app_api_client.dart';
 import 'package:azul_crm/core/storage/app_local_storage.dart';
+import 'package:azul_crm/features/auth/data/repositories/jwt_repository_interface.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt injector = GetIt.instance;
 
 Future<void> setupDependencies() async {
   // common
+  injector.registerLazySingleton(() => AppLogger());
   injector.registerLazySingleton(() => AppLocalStorage());
-  injector.registerSingleton(AppApiClient.instance);
 
+  // Auth
   await initAuthModule(injector);
+
+  // initAuthModule内でJWTRepositoryが生成されるため、後に実行
+  injector.registerLazySingleton(
+    () => AppApiClient(injector.get<JwtRepositoryInterface>()),
+  );
 }
