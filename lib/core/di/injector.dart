30c3b9043b7b08dfb22bd07de53648e7b404bdd0
azul <life.89.blue@gmail.com>
2026-01-14 10:02:23 -0300

    update/Register AppInitBloc and CheckIfAuthenticatedUsecase in dependency injection

--- lib/core/di/injector.dart
+++ lib/core/di/injector.dart
@@ -1,11 +1,16 @@
+import 'package:azul_crm/core/init/blocs/app_init_bloc.dart';
+import 'package:azul_crm/core/init/domains/usecases/check_if_authenticated_usecase.dart';
 import 'package:azul_crm/core/log/app_logger.dart';
 import 'package:azul_crm/core/network/app_api_client.dart';
 import 'package:azul_crm/core/storage/app_local_storage.dart';
+import 'package:azul_crm/features/auth/blocs/login/login_bloc.dart';
 import 'package:azul_crm/features/auth/data/repositories/jwt_repository_interface.dart';
 import 'package:azul_crm/features/auth/blocs/signup/signup_bloc.dart';
 import 'package:azul_crm/features/auth/data/repositories/auth_repository.dart';
 import 'package:azul_crm/features/auth/data/repositories/auth_repository_interface.dart';
 import 'package:azul_crm/features/auth/data/repositories/jwt_repository.dart';
+import 'package:azul_crm/features/auth/domains/usecases/login_usecase.dart';
+import 'package:azul_crm/features/auth/domains/usecases/logout_usecase.dart';
 import 'package:azul_crm/features/auth/domains/usecases/signup_usecase.dart';
 import 'package:azul_crm/features/my_page/blocs/invitation_code_bloc.dart';
 import 'package:azul_crm/features/my_page/data/repositories/invitation_code_repository.dart';
@@ -16,9 +21,8 @@ import 'package:azul_crm/features/my_page/domains/usecases/fetch_invitation_code
 import 'package:get_it/get_it.dart';
 
 part 'modules/auth_module.dart';
-part 'modules/my_page_module.dart';
 
-// Barrel Pattern に移行する
+part 'modules/my_page_module.dart';
 
 GetIt injector = GetIt.instance;
 
@@ -30,11 +34,16 @@ Future<void> setupDependencies() async {
   // Auth
   await initAuthModule(injector);
 
+  injector.registerLazySingleton(
+    () => AppApiClient(
+      injector.get<JwtRepositoryInterface>(),
+      injector.get<AppLocalStorage>(),
+    ),
+  );
+
   // MyPage
   await initMyPageModule(injector);
 
-  // initAuthModule内でJWTRepositoryが生成されるため、後に実行
-  injector.registerLazySingleton(
-    () => AppApiClient(injector.get<JwtRepositoryInterface>()),
-  );
+  injector.registerLazySingleton(() => CheckIfAuthenticatedUsecase(injector()));
+  injector.registerLazySingleton(() => AppInitBloc(injector()));
 }
