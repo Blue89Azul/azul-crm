d46d48ae4abf8990438c63adcf173223b695862e
azul <life.89.blue@gmail.com>
2026-01-14 10:00:36 -0300

    update/refactor AuthRepository and its interface to support account management and user data

--- lib/features/auth/data/repositories/auth_repository.dart
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -1,18 +1,28 @@
 import 'package:azul_crm/core/network/app_api_client.dart';
-import 'package:azul_crm/features/auth/data/dto/jwt_token.dart';
+import 'package:azul_crm/core/storage/app_local_storage.dart';
+import 'package:azul_crm/features/auth/data/dto/jwt/jwt_token.dart';
+import 'package:azul_crm/features/auth/data/dto/user/user.dart';
+import 'package:azul_crm/features/auth/domains/services/jwt_token_decoder.dart';
 import 'package:azul_crm/shared/roles/app_roles.dart';
 import 'package:dartz/dartz.dart';
 import 'auth_repository_interface.dart';
 
+const accountKey = "account";
+
 class AuthRepository implements AuthRepositoryInterface {
   final AppApiClient _apiClient;
+  final AppLocalStorage _storage;
+  final JwtTokenDecoder _jwtTokenDecoder;
 
-  AuthRepository(this._apiClient);
+
+  AuthRepository(this._apiClient, this._storage, this._jwtTokenDecoder);
 
   @override
-  Future<Either<String, JwtToken>> signup(
+  Future<Either<String, (JwtToken, User)>> signup(
     String email,
     String password,
+    String account,
+    String code,
     AppRole role,
   ) async {
     try {
@@ -20,25 +30,44 @@ class AuthRepository implements AuthRepositoryInterface {
       final requestData = {
         'email': email,
         'password': password,
+        'account': account,
+        'code': code,
         'role': role.name,
       };
 
       var result = await _apiClient.post('/signup', data: requestData);
-
-      return right(JwtToken.fromJson(result));
+      await _storage.write(key: accountKey, value: result['account']);
+      return right((
+        JwtToken.fromJson({
+          'token': result['jwt']['token'],
+          'expiresIn': _jwtTokenDecoder.toExpiredAt(result['jwt']['token'])
+        }),
+        User.fromJson(result['user']),
+      ));
     } catch (e) {
       return left(e.toString());
     }
   }
 
   @override
-  Future<Either<String, JwtToken>> login(String email, String password) async {
+  Future<Either<String, (JwtToken, User)>> login(
+    String email,
+    String password,
+    String account,
+  ) async {
     try {
-      final requestData = {'email': email, 'password': password};
+      final requestData = {
+        'email': email,
+        'password': password,
+        'account': account,
+      };
 
       var result = await _apiClient.post('/login', data: requestData);
-
-      return right(JwtToken.fromJson(result));
+      await _storage.write(key: accountKey, value: result['account']);
+      return right((
+        JwtToken.fromJson(result['jwt']),
+        User.fromJson(result['user']),
+      ));
     } catch (e) {
       return left(e.toString());
     }
@@ -48,9 +77,21 @@ class AuthRepository implements AuthRepositoryInterface {
   Future<Either<String, void>> logout() async {
     try {
       await _apiClient.post('/logout', data: {});
+      await _storage.delete(key: accountKey);
     } catch (e) {
       return left(e.toString());
     }
     return right(null);
+  }
+
+  @override
+  Future<Either<String, String>> getAccount() async {
+    late final String account;
+    try {
+      account = await _storage.read(key: accountKey);
+    } catch (e) {
+      return left(e.toString());
+    }
+    return right(account);
   }
 }
--- lib/features/auth/data/repositories/auth_repository_interface.dart
+++ lib/features/auth/data/repositories/auth_repository_interface.dart
@@ -1,9 +1,24 @@
-import 'package:azul_crm/features/auth/data/dto/jwt_token.dart';
+import 'package:azul_crm/features/auth/data/dto/jwt/jwt_token.dart';
+import 'package:azul_crm/features/auth/data/dto/user/user.dart';
 import 'package:azul_crm/shared/roles/app_roles.dart';
 import 'package:dartz/dartz.dart';
 
 abstract interface class AuthRepositoryInterface {
-  Future<Either<String, JwtToken>> signup(String email, String password, AppRole role);
-  Future<Either<String, JwtToken>> login(String email, String password);
+  Future<Either<String, (JwtToken, User)>> signup(
+    String email,
+    String password,
+    String account,
+    String code,
+    AppRole role,
+  );
+
+  Future<Either<String, (JwtToken, User)>> login(
+    String email,
+    String password,
+    String account,
+  );
+
   Future<Either<String, void>> logout();
+
+  Future<Either<String, String>> getAccount();
 }
