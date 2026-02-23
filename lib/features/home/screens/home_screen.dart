9e67ff2632594e38c2ffa4a5408a4e4f9f17f064
azul <life.89.blue@gmail.com>
2026-02-23 10:44:42 -0300

    refactor: convert HomeScreen to StatelessWidget and integrate StatefulNavigationShell for navigation

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,26 +1,39 @@
 import 'package:azul_crm/core/router/app_router.dart';
 import 'package:azul_crm/shared/user/logged_in_user.dart';
-import 'package:azul_crm/features/home/screens/dashboard_view.dart';
 import 'package:azul_crm/shared/widgets/screen_padding.dart';
 import 'package:azul_crm/theme/app_text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  final LoggedInUser _loggedInUser = LoggedInUser();
+  final StatefulNavigationShell navigationShell;
+
+  void onTapAppBarMenu(BuildContext context) {
+    context.pushNamed(AppRouteNames.configuration);
+  }
+
+  HomeScreen({super.key, required this.navigationShell});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  Widget build(BuildContext context) {
+    return _HomeView(_loggedInUser, navigationShell, onTapAppBarMenu);
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  int _currentPageIndex = 0;
-  final LoggedInUser _loggedInUser = LoggedInUser();
+class _HomeView extends StatelessWidget {
+  late final LoggedInUser _loggedInUser;
+  late final StatefulNavigationShell _navigationShell;
+  late final Function(BuildContext context) _onTapAppBarMenu;
 
-  void _onTapBottomNavigation(int index) {
-    setState(() {
-      _currentPageIndex = index;
-    });
+  _HomeView(
+    LoggedInUser loggedInUser,
+    StatefulNavigationShell navigationShell,
+    Function(BuildContext context) onTapAppBarMenu,
+  ) {
+    _loggedInUser = loggedInUser;
+    _navigationShell = navigationShell;
+    _onTapAppBarMenu = onTapAppBarMenu;
   }
 
   @override
@@ -29,72 +42,52 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('Dashboard', style: AppTextStyle.heading1),
         leading: IconButton(
-          onPressed: () {
-            context.pushNamed(AppRouteNames.configuration);
-          },
+          onPressed: _onTapAppBarMenu(context),
           icon: Icon(Icons.menu),
         ),
         actions: [
           if (_loggedInUser.appRole != null)
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                children: [
-                  Icon(
-                    _loggedInUser.isAdmin 
-                        ? Icons.admin_panel_settings 
-                        : Icons.person,
-                    color: _loggedInUser.isAdmin 
-                        ? Colors.amber 
-                        : Colors.blue,
-                  ),
-                  SizedBox(width: 8),
-                  Text(
-                    _loggedInUser.isAdmin ? 'Admin' : 'Member',
-                    style: AppTextStyle.bodyMedium.copyWith(
-                      color: _loggedInUser.isAdmin 
-                          ? Colors.amber 
-                          : Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
+              child: _roleIcon(_loggedInUser),
             ),
         ],
       ),
-      body: ScreenPadding(
-        child: <Widget>[
-          DashBoardView(),
-          Center(child: Text('CustomerPage')),
-          Center(child: Text('CompanyPage')),
-          Center(child: Text('ActionsPage')),
-        ][_currentPageIndex],
-      ),
+      body: ScreenPadding(child: _navigationShell),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentPageIndex,
-        onTap: _onTapBottomNavigation,
+        currentIndex: _navigationShell.currentIndex,
         selectedItemColor: Theme.of(context).primaryColor,
         unselectedItemColor: Colors.black26,
-        items: <BottomNavigationBarItem>[
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.dashboard),
-            label: 'Dashboard',
+            label: AppRouteNames.dashboard,
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.supervised_user_circle),
-            label: 'Customers',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_work_rounded),
-            label: 'Companies',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.pending_actions),
-            label: 'Actions',
+            label: AppRouteNames.customer,
           ),
         ],
       ),
     );
   }
+}
+
+Widget _roleIcon(LoggedInUser loggedInUser) {
+  return Row(
+    children: [
+      Icon(
+        loggedInUser.isAdmin ? Icons.admin_panel_settings : Icons.person,
+        color: loggedInUser.isAdmin ? Colors.amber : Colors.blue,
+      ),
+      SizedBox(width: 8),
+      Text(
+        loggedInUser.getRoleName(),
+        style: AppTextStyle.bodyMedium.copyWith(
+          color: loggedInUser.isAdmin ? Colors.amber : Colors.blue,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ],
+  );
 }
